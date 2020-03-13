853716190b3ab3398565d62987ce641f055a6dbd
jdlight <54721683+jacob-light@users.noreply.github.com>
2020-03-13 15:46:53 -0700

    Update description, namespace, and create man folder.

--- R/pop.R
+++ R/pop.R
@@ -3,7 +3,7 @@
 #' Contains processed county-year-level population
 #' estimates downloaded from the Census.
 #'
-#' @name bitly
+#' @name pop
 #' @docType data
 #' @references [BLS Census
 #'     Data](https://www2.census.gov/programs-surveys/popest/datasets/2010-2018/counties/asrh/cc-est2018-alldata-06.csv)
